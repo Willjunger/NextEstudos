4d23cb566a0d0c0a63f6c2bdf65d5a7d18c1cb74
Willjunger <williamjunger@hotmail.com>
2024-03-25 22:38:33 -0300

    adiciona Link para alterar as paginas

--- pages/faq.js
+++ pages/faq.js
@@ -1,8 +1,10 @@
+import Link from "next/link";
+
 export default function FAQPage() {
   return (
     <div>
       <h1>Alura Cases - Páginas de perguntas FAQ</h1>
-      <a href="/">Ir para a home</a>
+      <Link href="/">Ir para a home</Link>
     </div>
   );
 }
