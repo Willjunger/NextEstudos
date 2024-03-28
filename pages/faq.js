d9086422d281dac3e72da858e332fc350156324a
Willjunger <williamjunger@hotmail.com>
2024-03-27 23:59:47 -0300

    entendendo getServerSideProps

--- pages/faq.js
+++ pages/faq.js
@@ -1,7 +1,12 @@
-import { useEffect, useState } from "react";
 import Link from "../src/components/Link";
 
+// export async function getServerSideProps() {
+// console.log("EM MODO DEV, SEMPRE TODA! A CADA ACESSO");
+//   console.log("RODA A CADA ACESSO QUE VOCÊ RECEBE");
+// }
 export async function getStaticProps() {
+  console.log("EM MODO DEV, SEMPRE TODA! A CADA ACESSO");
+  console.log("RODA SOMENTE EM BUILD TIME");
   const FAQ_API_URL =
     "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json";
   const faq = await fetch(FAQ_API_URL)
